cc7532201dfc9056c9795322bf2d5cf13f2aa9a8
Bill Chan <billypchan@gmail.com>
2021-06-14 14:06:35 +0200

    split into objc and swift target

--- Package.swift
+++ Package.swift
@@ -16,16 +16,20 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+           name: "WireSystem-ObjC",
+           dependencies: [],
+           path: "WireSystem/",
+           exclude: ["SwiftSources"],
+           cSettings: [
+//              .headerSearchPath("Internal"), // 5
+           ]
+        ),
         .target(
             name: "WireSystem",
-            dependencies: [],
-            publicHeadersPath: "include",
-            cSettings: [
-                            .headerSearchPath("."),
-                            .headerSearchPath("Private"),
-            ]),
+            dependencies: ["WireSystem-ObjC"],
+            path: "SwiftSources"
+        ),
         .testTarget(
             name: "WireSystem Tests",
             dependencies: ["WireSystem"]),
